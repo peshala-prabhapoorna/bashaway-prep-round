b583fca103cfe84c1e6ed2c4111eb87a04084a66
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-09-15 09:17:55 +0530

    solution to Clockwise

--- scriptville/execute.sh
+++ scriptville/execute.sh
@@ -3,7 +3,7 @@
 mkdir -p out
 for filename in src/*; do
     if test "$filename" = "src/*"; then
-        echo "folder is empty of doesn't exist"
+        echo "folder is empty or doesn't exist"
         break
     fi
     convert "$filename" -resize 400% "out/"${filename:4}
